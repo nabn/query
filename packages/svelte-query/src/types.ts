0cd70692443765f38278459a8d09fdf3d1b7965c
Zhiheng Zhang <55773069+zhihengGet@users.noreply.github.com>
2024-07-25 18:13:35 -0700

    WIP: Svelte 5 adapter (#6981)
    docs(svelte-5-adapter): force runes mode for all examples (#7800)
    
    * docs(examples): force runes mode for all examples
    
    * Fix simple example mounting
    
    * Fix type
    feat(svelte-5-adapter): require options to be passed as function (#7804)
    
    * fix(svelte-5-adapter): Require options to be passed as function
    
    * More fixes
    
    * More fixes
    
    * Update examples
    
    fix(svelte-5-adapter): function for `createMutation` options (#7805)
    
    * fix(svelte-5-adapter): require function for createMutation options
    
    * Fix type errors
    refactor(svelte-5-adapter): simplify createBaseQuery (#7808)
    
    * refactor: simplify createBaseQuery
    
    * Don't take snapshot of queryKey
    chore(svelte-5-adapter): tidy-up functions (#7809)
    
    * chore(svelte-5-adapter): tidy-up functions
    
    * Fix types
    
    * Update svelte
    
    Fix import extensions
    
    chore: ignore state_snapshot_uncloneable in tests

--- packages/svelte-query/src/types.ts
+++ packages/svelte-query/src/types.ts
@@ -1,3 +1,4 @@
+import type { Snippet } from 'svelte'
 import type {
   DefaultError,
   DefinedQueryObserverResult,
@@ -9,15 +10,13 @@ import type {
   MutationObserverOptions,
   MutationObserverResult,
   MutationState,
-  OmitKeyof,
+  QueryClient,
   QueryKey,
   QueryObserverOptions,
   QueryObserverResult,
 } from '@tanstack/query-core'
-import type { Readable } from 'svelte/store'
 
-/** Allows a type to be either the base object or a store of that object */
-export type StoreOrVal<T> = T | Readable<T>
+export type FunctionedParams<T> = () => T
 
 /** Options for createBaseQuery */
 export type CreateBaseQueryOptions<
@@ -32,7 +31,7 @@ export type CreateBaseQueryOptions<
 export type CreateBaseQueryResult<
   TData = unknown,
   TError = DefaultError,
-> = Readable<QueryObserverResult<TData, TError>>
+> = QueryObserverResult<TData, TError>
 
 /** Options for createQuery */
 export type CreateQueryOptions<
@@ -69,13 +68,13 @@ export type CreateInfiniteQueryOptions<
 export type CreateInfiniteQueryResult<
   TData = unknown,
   TError = DefaultError,
-> = Readable<InfiniteQueryObserverResult<TData, TError>>
+> = InfiniteQueryObserverResult<TData, TError>
 
 /** Options for createBaseQuery with initialData */
 export type DefinedCreateBaseQueryResult<
   TData = unknown,
   TError = DefaultError,
-> = Readable<DefinedQueryObserverResult<TData, TError>>
+> = DefinedQueryObserverResult<TData, TError>
 
 /** Options for createQuery with initialData */
 export type DefinedCreateQueryResult<
@@ -89,9 +88,9 @@ export type CreateMutationOptions<
   TError = DefaultError,
   TVariables = void,
   TContext = unknown,
-> = OmitKeyof<
+> = Omit<
   MutationObserverOptions<TData, TError, TVariables, TContext>,
-  '_defaulted'
+  '_defaulted' | 'variables'
 >
 
 export type CreateMutateFunction<
@@ -128,7 +127,7 @@ export type CreateMutationResult<
   TError = DefaultError,
   TVariables = unknown,
   TContext = unknown,
-> = Readable<CreateBaseMutationResult<TData, TError, TVariables, TContext>>
+> = CreateBaseMutationResult<TData, TError, TVariables, TContext>
 
 type Override<TTargetA, TTargetB> = {
   [AKey in keyof TTargetA]: AKey extends keyof TTargetB
@@ -142,4 +141,9 @@ export type MutationStateOptions<TResult = MutationState> = {
   select?: (
     mutation: Mutation<unknown, DefaultError, unknown, unknown>,
   ) => TResult
+}
+
+export type QueryClientProviderProps = {
+  client: QueryClient
+  children: Snippet
 }
